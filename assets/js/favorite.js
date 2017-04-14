85af89dac8e70bd02b753d59b2c8521dd118450e
Yu-Jen Lin <yjlin5210@gmail.com>
2017-04-14 17:50:04 -0400

    update favorite, add unsubscribe in favorite page

--- assets/js/favorite.js
+++ assets/js/favorite.js
@@ -11,15 +11,33 @@ function updateFavorite() {
     var allcomics = settings.get("comic");
     for (host in allcomics) {
         for (comic in allcomics[host]) {
-            console.log ("host:" + comic);
+            if (!allcomics[host][comic].subscribed) {
+                continue;
+            }
+            // console.log ("host:" + comic);
             // create favorite comic panel
-            var obj = $("<div class='pure-u-1-12 favorite-entry' title='" 
+            var obj = $("<div class='pure-u-4-24 favorite-entry' title='" 
                 + allcomics[host][comic].title + "' + link='" 
                 + allcomics[host][comic].link + "' ></div>");
-            var imgObj = $("<div class='pure-u-1 img-thumb'><img src='" 
+            var imgObj = $("<div class='pure-u-10-24'><img src='" 
                 + allcomics[host][comic].thumbnail + "'></div>");
             obj.append(imgObj);
-            obj.append("<div class='pure-u-1 comic-title'>" + allcomics[host][comic].title + "</div>");
+            var desObj = $("<div class='pure-u-14-24'></div>");
+            desObj.append("<h2 class='comic-title'>" + allcomics[host][comic].title + "</h2>");
+            desObj.append("<p class='comic-host'>from: " + host + "</p>");
+            obj.append(desObj);
+            
+            var unsubscribeBtn = $("<div class='unsubscribe'>x</div>");
+            (function(host, comic) {
+                unsubscribeBtn.click(function(e) {
+                    e.stopPropagation();
+                    console.log("comic." + host + "." + comic + ".subscribed");
+                    settings.set("comic." + host + "." + comic + ".subscribed", false);
+                    updateFavorite();
+                });
+            })(host, comic);
+            obj.append(unsubscribeBtn);
+            
             obj.click(selectComic);
 
             $('#favorite-list').append(obj);
