a92e7315ec998db9409f03b7626d222ac2938348
BEE <bee@rakuya.com.tw>
2017-05-05 13:32:51 +0800

    mobile-view style fix

--- renderer-process/viewcontrollers/read-viewcontroller.js
+++ renderer-process/viewcontrollers/read-viewcontroller.js
@@ -283,7 +283,7 @@ function init() {
 function lateInit() {
     // comic header click behavior in mobile view
     $("#comic-header").click(function(e) {
-        if ($("#comic-header").css("top") == "50px") {
+        if ($("#comic-header").css("top") == "85px") {
             // toggle chapter selector
             toggleChapterSelector();
         }
@@ -441,8 +441,11 @@ function toggleChapterSelector() {
     var chapter_selector = $("#chapter-selector");
     if (chapter_selector.hasClass("is-hidden-mobile")) {
         chapter_selector.removeClass("is-hidden-mobile");
+        $('.zoom-btn').css("z-index","2");
+
     } else {
         chapter_selector.addClass("is-hidden-mobile");
+        $('.zoom-btn').css("z-index","99");
     }
 }
 
