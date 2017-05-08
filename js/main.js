a0271e14daccbc076e86ab2e0e1794a88b6f5ec0
fuluboy <muzicbee@gmail.com>
2017-05-08 23:28:23 +0800

    Landing Page v1.1

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-
 $(document).ready(function() {
+  // MENU BG COLOR
   $(window).scroll(function() {
     if ($(window).scrollTop() > 100) {
-          $('.home-menu').addClass('active');
+      $('.home-menu').addClass('active');
+    } else {
+      $('.home-menu').removeClass('active');
     }
-    else {
-        $('.home-menu').removeClass('active');
-    }
   });
 
-
-  $( "#btn01" ).click(function() {
-    $('html, body').animate({scrollTop: $('#feature').offset().top - 50}, 'slow');
+  // MENU FUNCTION
+  $("#btn01").click(function() {
+    $('html, body').animate({
+      scrollTop: $('#feature').offset().top - 63
+    }, 'slow');
   });
-  $( "#btn02" ).click(function() {
-    $('html, body').animate({scrollTop: $('#getstarted').offset().top - 50}, 'slow');
+  $("#btn02").click(function() {
+    $('html, body').animate({
+      scrollTop: $('#getstarted').offset().top - 63
+    }, 'slow');
   });
-  $( "#btn04" ).click(function() {
-    $('html, body').animate({scrollTop: $('#download').offset().top - 50}, 'slow');
+  $("#btn04").click(function() {
+    $('html, body').animate({
+      scrollTop: $('#download').offset().top - 63
+    }, 'slow');
   });
-
-
 
 });
