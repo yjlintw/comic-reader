778f34774ff3e95eb2cf341a878dd5bf1e4c3dc4
Yu-Jen Lin <yjlin5210@gmail.com>
2017-05-28 23:16:36 +0800

    add basic keyboard shortcut

--- main-process/menu/mainmenu.js
+++ main-process/menu/mainmenu.js
@@ -47,7 +47,18 @@ if (process.platform === 'darwin') {
             { type: 'separator' },
             { role: 'quit' }
         ]
-    }]
+    }, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ]
     const menu = Menu.buildFromTemplate(template)
     Menu.setApplicationMenu(menu)
 }
